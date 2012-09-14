34984d90c0a6e19c1f5a8244af8d2cb8c6bc3eea
ymotongpoo <ymotongpoo@gmail.com>
2012-09-14 21:28:23 +0900

    fixed repository title

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# docs.ymotongpoo.com documentation build configuration file, created by
+# tech.ymotongpoo.com documentation build configuration file, created by
 # sphinx-quickstart on Fri Sep 14 20:55:46 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'docs.ymotongpoo.com'
+project = u'tech.ymotongpoo.com'
 copyright = u'2012, Yoshifumi YAMAGUCHI (@ymotongpoo)'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -106,14 +106,14 @@ html_theme = 'bootstrap'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "docs.ymotongpoo.com"
+html_short_title = "tech.ymotongpoo.com"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 html_logo = "ymotongpoo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# tech.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
 
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'docsymotongpoocomdoc'
+htmlhelp_basename = 'techymotongpoocomdoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'docsymotongpoocom.tex', u'docs.ymotongpoo.com Documentation',
+  ('index', 'techymotongpoocom.tex', u'tech.ymotongpoo.com Documentation',
    u'Yoshifumi YAMAGUCHI (@ymotongpoo)', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'docsymotongpoocom', u'docs.ymotongpoo.com Documentation',
+    ('index', 'techymotongpoocom', u'tech.ymotongpoo.com Documentation',
      [u'Yoshifumi YAMAGUCHI (@ymotongpoo)'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'docsymotongpoocom', u'docs.ymotongpoo.com Documentation',
-   u'Yoshifumi YAMAGUCHI (@ymotongpoo)', 'docsymotongpoocom', 'One line description of project.',
+  ('index', 'techymotongpoocom', u'tech.ymotongpoo.com Documentation',
+   u'Yoshifumi YAMAGUCHI (@ymotongpoo)', 'techymotongpoocom', 'One line description of project.',
    'Miscellaneous'),
 ]
 
--- source/index.rst
+++ source/index.rst
@@ -1,10 +1,10 @@
-.. docs.ymotongpoo.com documentation master file, created by
+.. tech.ymotongpoo.com documentation master file, created by
    sphinx-quickstart on Fri Sep 14 20:55:46 2012.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to docs.ymotongpoo.com's documentation!
-===============================================
+tech.ymotongpoo.com
+===================
 
 Contents:
 
